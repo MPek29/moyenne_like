e6a3c30850f78bb2b2e27e2b9da60019da0f33c5
MPek29 <florianpasco@protonmail.com>
2022-09-05 20:36:25 +0200

    V0.2.2 : Creation of the SubjectActivity layout

---
+++ app/src/main/java/fr/palp15/moyenne_like/SubjectActivity.kt
@@ -0,0 +1,11 @@
+package fr.palp15.moyenne_like
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subject)
+    }
+}
